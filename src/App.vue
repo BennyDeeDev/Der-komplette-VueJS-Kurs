2fe69a077152e8fbd4e7eefcd269ea360f6af356
b.derksen@mail.de <b.derksen@mail.de>
2020-05-14 20:42:35 +0200

    Lektion 7: Components

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,15 @@
 <template>
   <div>
-    <div v-bind:style="{color: activeColor}" v-if="showMessage">{{ message }}</div>
-    <div v-for="(todo,index) in todos" :key="index">{{ todo.text }}</div>
-    <button v-on:click="addToDo">Hinzufügen</button>
-    <input v-model="message" />
-    <button v-on:click="resetToDo">RESET</button>
+    <ToDoList />
   </div>
 </template>
 
 <script>
+import ToDoList from "@/components/ToDoList.vue";
 //https://stackoverflow.com/questions/42260233/vue-js-difference-between-v-model-and-v-bind
 export default {
-  data() {
-    return {
-      message: "Hello Vue!",
-      activeColor: "green",
-      showMessage: true,
-      todos: [
-        { text: "HTML lernen" },
-        { text: "JavaScript lernen" },
-        { text: "Vue lernen" }
-      ]
-    };
-  },
-  methods: {
-    addToDo() {
-      this.todos.push({ text: "Eine geile ToDo App bauen" });
-    },
-    resetToDo() {
-      this.todos = [];
-    }
+  components: {
+    ToDoList
   }
 };
 </script>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <input v-model="message" />
+    <div v-bind:style="{color: activeColor}" v-if="showMessage">{{ message }}</div>
+    <div v-for="(todo,index) in todos" :key="index">{{ todo.text }}</div>
+    <button v-on:click="addToDo">Hinzufügen</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello Vue!",
+      activeColor: "green",
+      showMessage: true,
+      todos: [
+        { text: "HTML lernen" },
+        { text: "JavaScript lernen" },
+        { text: "Vue lernen" }
+      ]
+    };
+  },
+  methods: {
+    addToDo() {
+      this.todos.push({ text: "Eine geile ToDo App bauen" });
+    }
+  }
+};
+</script>
+
+<style>
+</style>
