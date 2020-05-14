e10fcecc4f230eabfa13c39b98813d929e4e6157
b.derksen@mail.de <b.derksen@mail.de>
2020-05-14 21:12:24 +0200

    Lektion 9:  DeleteToDo

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <input @change="addToDo" />
-    <div v-for="(todo,index) in todos" :key="index">{{ todo.text }}</div>
+    <div v-for="(todo,index) in todos" :key="index">
+      {{ todo.text }}
+      <button v-on:click="deleteToDo(todo.id)">X</button>
+    </div>
   </div>
 </template>
 
@@ -10,15 +13,22 @@ export default {
   data() {
     return {
       todos: [
-        { text: "HTML & CSS lernen" },
-        { text: "JavaScript lernen" },
-        { text: "Vue lernen" }
-      ]
+        { id: 1, text: "HTML & CSS lernen" },
+        { id: 2, text: "JavaScript lernen" },
+        { id: 3, text: "Vue lernen" }
+      ],
+      counter: 4
     };
   },
   methods: {
     addToDo(e) {
-      this.todos.push({ text: e.target.value });
+      this.todos.push({ id: this.counter++, text: e.target.value });
+    },
+    deleteToDo(id) {
+      this.todos.splice(
+        this.todos.findIndex(item => item.id === id),
+        1
+      );
     }
   }
 };
