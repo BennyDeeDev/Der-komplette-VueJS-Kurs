2a18cd204388e4c2a8b7d323b108e8b54ec99a35
b.derksen@mail.de <b.derksen@mail.de>
2020-05-14 20:44:48 +0200

    Lektion 7: Components

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <input v-model="message" />
-    <div v-bind:style="{color: activeColor}" v-if="showMessage">{{ message }}</div>
     <div v-for="(todo,index) in todos" :key="index">{{ todo.text }}</div>
     <button v-on:click="addToDo">Hinzufügen</button>
   </div>
@@ -11,9 +10,6 @@
 export default {
   data() {
     return {
-      message: "Hello Vue!",
-      activeColor: "green",
-      showMessage: true,
       todos: [
         { text: "HTML lernen" },
         { text: "JavaScript lernen" },
