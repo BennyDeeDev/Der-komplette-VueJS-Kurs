50c3e30e1c08166fd446007754c099c6a53c10f5
b.derksen@mail.de <b.derksen@mail.de>
2020-05-16 16:05:25 +0200

    NavBar Routing fixed

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <h1> <router-link to="{name: ToDoList}"> ToDos</h1>
-    <h1> <router-link to="{name: Login}"> Login</h1>
+    <h1>
+      <router-link to="{name: ToDoList}">ToDos</router-link>
+    </h1>
+    <h1>
+      <router-link to="{name: Login}">Login</router-link>
+    </h1>
   </div>
 </template>
 
