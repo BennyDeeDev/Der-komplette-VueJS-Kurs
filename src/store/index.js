8f44627a3aa016d37dcba29cf90d8e962874f384
b.derksen@mail.de <b.derksen@mail.de>
2020-05-16 19:48:35 +0200

    fetchToDos

--- src/store/index.js
+++ src/store/index.js
@@ -1,41 +1,45 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import ToDoService from "@/services/ToDoService";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    todos: [
-      { id: 1, title: "HTML & CSS lernen", done: true },
-      { id: 2, title: "JavaScript lernen", done: true },
-      { id: 3, title: "Vue lernen", done: false },
-    ],
-    counter: 4,
-    searchQuery: "",
-  },
-  mutations: {
-    ADD_TODO(state, title) {
-      state.todos.push({ id: state.counter++, title });
+    state: {
+        todos: [],
+        counter: 4,
+        searchQuery: "",
     },
-    DELETE_TODO(state, id) {
-      state.todos.splice(
-        state.todos.findIndex((item) => item.id === id),
-        1
-      );
+    mutations: {
+        ADD_TODO(state, title) {
+            state.todos.push({ id: state.counter++, title });
+        },
+        DELETE_TODO(state, id) {
+            state.todos.splice(
+                state.todos.findIndex((item) => item.id === id),
+                1
+            );
+        },
+        SET_TODOS(state, data) {
+            state.todos = data;
+        },
     },
-  },
-  actions: {},
-  getters: {
-    activeToDos: (state) => {
-      return state.todos.filter((item) => !item.done);
+    actions: {
+        fetchToDos({ commit }) {
+            ToDoService.getToDos()
+                .then(({ data }) => commit("SET_TODOS", data))
+                .catch((error) => console.log(error));
+        },
     },
-    doneToDos: (state) => {
-      return state.todos.filter((item) => item.done);
+    getters: {
+        activeToDos: (state) => {
+            return state.todos.filter((item) => !item.done);
+        },
+        doneToDos: (state) => {
+            return state.todos.filter((item) => item.done);
+        },
+        filterToDos: (state) => {
+            return state.todos.filter((item) => item.title.toLowerCase().startsWith(state.searchQuery.toLowerCase()));
+        },
     },
-    filterToDos: (state) => {
-      return state.todos.filter((item) =>
-        item.title.toLowerCase().startsWith(state.searchQuery.toLowerCase())
-      );
-    },
-  },
 });
--- src/views/ToDoList.vue
+++ src/views/ToDoList.vue
@@ -1,45 +1,47 @@
 <template>
-  <div>
-    <h2>Search To Dos</h2>
-    <input v-model="$store.state.searchQuery" />
-    <div v-if="$store.state.searchQuery">
-      <ToDoItem v-for="todo in getFilteredToDos" :key="todo.id" v-bind:todo="todo" />
+    <div>
+        <h2>Search To Dos</h2>
+        <input v-model="$store.state.searchQuery" />
+        <div v-if="$store.state.searchQuery">
+            <ToDoItem v-for="todo in getFilteredToDos" :key="todo.id" v-bind:todo="todo" />
+        </div>
+
+        <h2>Add To Do</h2>
+        <input @change="addToDo" />
+
+        <h2>Active ToDos</h2>
+        <ToDoItem v-for="todo in getActiveToDos" :key="todo.id" v-bind:todo="todo" />
+        <h2>Done ToDos</h2>
+        <ToDoItem v-for="todo in getDoneToDos" :key="todo.id" v-bind:todo="todo" />
     </div>
-
-    <h2>Add To Do</h2>
-    <input @change="addToDo" />
-
-    <h2>Active ToDos</h2>
-    <ToDoItem v-for="todo in getActiveToDos" :key="todo.id" v-bind:todo="todo" />
-    <h2>Done ToDos</h2>
-    <ToDoItem v-for="todo in getDoneToDos" :key="todo.id" v-bind:todo="todo" />
-  </div>
 </template>
 
 <script>
 import ToDoItem from "@/components/ToDoItem.vue";
 export default {
-  components: {
-    ToDoItem
-  },
-  methods: {
-    addToDo(e) {
-      this.$store.commit("ADD_TODO", e.target.value);
-    }
-  },
-  computed: {
-    getActiveToDos() {
-      return this.$store.getters.activeToDos;
+    components: {
+        ToDoItem,
     },
-    getDoneToDos() {
-      return this.$store.getters.doneToDos;
+    methods: {
+        addToDo(e) {
+            this.$store.dispatch("ADD_TODO", e.target.value);
+        },
     },
-    getFilteredToDos() {
-      return this.$store.getters.filterToDos;
-    }
-  }
+    created() {
+        this.$store.dispatch("fetchToDos");
+    },
+    computed: {
+        getActiveToDos() {
+            return this.$store.getters.activeToDos;
+        },
+        getDoneToDos() {
+            return this.$store.getters.doneToDos;
+        },
+        getFilteredToDos() {
+            return this.$store.getters.filterToDos;
+        },
+    },
 };
 </script>
 
-<style>
-</style>
+<style></style>
