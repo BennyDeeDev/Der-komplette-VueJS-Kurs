9de6d8b3fe131003a6cfa5d046e3612b6c1ff9b0
b.derksen@mail.de <b.derksen@mail.de>
2020-05-16 16:02:03 +0200

    router view und router.js

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,12 @@
 <template>
   <div>
-    <ToDoList />
+    <router-view />
   </div>
 </template>
 
 <script>
-import ToDoList from "@/components/ToDoList.vue";
 //https://stackoverflow.com/questions/42260233/vue-js-difference-between-v-model-and-v-bind
-export default {
-  components: {
-    ToDoList
-  }
-};
+export default {};
 </script>
 
 <style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,22 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import ToDoList from "@/components/ToDoList.vue";
+import Login from "@/components/Login.vue";
 
 Vue.use(VueRouter);
 
-const routes = [];
+const routes = [
+  {
+    component: ToDoList,
+    name: "ToDoList",
+    path: "/",
+  },
+  {
+    component: Login,
+    name: "Login",
+    path: "/login",
+  },
+];
 
 const router = new VueRouter({
   mode: "history",
